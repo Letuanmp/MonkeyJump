215a1011cdf075866193d56194d3985cbe52b818
Letuanmp <92443034+Letuanmp@users.noreply.github.com>
2023-07-19 17:33:23 +0700

    Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public Player player;
     public Spawner spawner;
-    public Text scoreText; 
+    public Text scoreText;
+
+    public GameObject gameOver;
+    public GameObject newGame;
+    public GameObject resetGame;
+
 
     private int score = 0; // Điểm số hiện tại
 
@@ -29,17 +34,31 @@ public class GameManager : MonoBehaviour
     private void Pause()
     {
         
-
         Time.timeScale = 0f; // Đặt Time.timeScale thành 0
     }
     public void NewGame()
     {
-        player.gameObject.SetActive(true);
-        spawner.gameObject.SetActive(true);
+        gameOver.SetActive(false);
+        newGame.SetActive(false);
+        resetGame.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        
+    }
+    public void Back()
+    {
+        Application.LoadLevel("PlayScreen");
     }
     public void GameOver()
     {
+        Wait();
         Pause();
+        gameOver.SetActive( true);
+        newGame.SetActive( true);
+        resetGame.SetActive( true);
     }
 
     public void Point()
